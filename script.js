e51eed1973219036d9aa43fc9463c7af43387aa8
Amish410 <amishsingh410@gmail.com>
2025-06-23 17:51:46 -0700

    fun facts

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function randompotato(potato) {
-    fetch('potato_data.json')
-        .then(response => response.json())
-        .then (data => {
-            const randomimg = data[Math.floor(Math.random() * data.length)];
-            document.getElementById("mainPotato").src = randomimg.image_url;
-            potatoName = randomimg.name
-            document.getElementById("potatoName").textContent = `This is the ${potatoName} potato!`;
-        }) 
+let potatoData = [];
+let randomimg = null;
+
+fetch('potato_data.json')
+    .then(response => response.json())
+    .then(data => {
+        potatoData = data;
+    })
+
+function randompotato() {
+    randomimg = potatoData[Math.floor(Math.random() * potatoData.length)];
+    document.getElementById("mainPotato").src = randomimg.image_url;
+    potatoName = randomimg.name;
+    document.getElementById("potatoName").textContent = `This is the ${potatoName} potato!`;
+}
+
+function funfacts() {
+    let potatoFact = randomimg.fun_fact;
+    document.getElementById("potatoFact").textContent = `${potatoFact}`;
 }
